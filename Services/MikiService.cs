34e612adead88608c1b66a58332f3777d2ee5763
an-geffen <an.geffen@alkerem.com>
2024-11-24 11:52:13 +0200

    Added gettiing connection string

--- Services/MikiService.cs
+++ Services/MikiService.cs
@@ -1,6 +1,19 @@
-﻿namespace miki_practice_api.Services
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Collections.Generic; //for list<T>
+
+namespace miki_practice_api.Services
 {
+    // Service to handle database operations.
     public class MikiService
     {
+        //Get the connection string 
+        private readonly string connectionString;
+
+        public MikiService(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+
     }
 }
