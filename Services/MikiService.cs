f0f0bbf7ecbffb4b77fd4a1419a32106f855d26d
an-geffen <an.geffen@alkerem.com>
2024-11-24 16:02:34 +0200

    connecting to oracle code

--- Services/MikiService.cs
+++ Services/MikiService.cs
@@ -1,4 +1,5 @@
 ﻿using Oracle.ManagedDataAccess.Client;
+using miki_practice_api.Models;
 using System.Collections.Generic; //for list<T>
 
 namespace miki_practice_api.Services
@@ -9,11 +10,20 @@ namespace miki_practice_api.Services
         //Get the connection string 
         private readonly string connectionString;
 
+
+        // Constructor to initialize the connection string.
         public MikiService(IConfiguration configuration)
         {
             connectionString = configuration.GetConnectionString("DefaultConnection")!; //"!" = The connection string is not null
         }
 
-
+        public List<MikiModel> GetAll()
+        {
+            var records = new List<MikiModel>();
+            using (var connection =  new OracleConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
     }
 }
