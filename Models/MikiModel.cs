06bb2275472158f9a66d93b8147cf930ea5a63e5
an-geffen <an.geffen@alkerem.com>
2024-11-24 14:12:24 +0200

    Add data model for item representation

---
+++ Models/MikiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace miki_practice_api.Models
+{
+    public class MikiModel // Represents a single record from the database.
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;//This ensures Name always has a value (string.Empty is a placeholder). Instead of using string?
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
