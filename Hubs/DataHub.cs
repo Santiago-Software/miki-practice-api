847f9fb3ff213c211a3cc5c81e15b3b0485bf67f
an-geffen <an.geffen@alkerem.com>
2024-11-25 14:25:51 +0200

    Add Hub code

---
+++ Hubs/DataHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace miki_practice_api.Hubs
+{
+    public class DataHub : Hub
+    {
+        // Example method for broadcasting data to all connected clients
+        public async Task BroadcastData(string data)
+        {
+            // Sends data to all connected clients
+            await Clients.All.SendAsync("ReceiveData", data); // "ReceiveData" is the client-side method name.
+        }
+    }
+}
